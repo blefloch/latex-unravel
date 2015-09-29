d5d2560058c0ed4eaea242c226803301e4e8f735
Bruno Le Floch <blflatex@gmail.com>
2015-09-29 21:00:08 +0200

    Test pTeX and upTeX

--- build.lua
+++ build.lua
@@ -1,6 +1,7 @@
 #!/usr/bin/env texlua
 -- Build script for unravel
 module = "unravel"
+checkengines = {"pdftex", "luatex", "xetex", "ptex", "uptex"}
 
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
